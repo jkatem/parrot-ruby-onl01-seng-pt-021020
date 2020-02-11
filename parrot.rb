dce60e8a7e431dccd38e46c915f931267f9beb4d
Jen Miao <jenmiao@gmail.com>
2020-02-11 17:18:35 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,16 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+# def parrot(phrase = "Pretty bird!")
+#   return "Pretty bird!"
+#   puts "Squawk!"
+# end 
+
+def parrot(phrase = "Pretty bird!")
+  puts phrase
+  phrase
+end
+
+
+
+
